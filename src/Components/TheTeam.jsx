15627759402764f6d0a4699d5adc31645c7eb348
Lekan <abdulmalikawesu@gmail.com>
2024-09-23 18:46:29 +0100

    added owners images

--- src/Components/TheTeam.jsx
+++ src/Components/TheTeam.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ceo from '/public/ceo.jpg'
+import headlegal from '/public/headlegal.jpg'
 
 const TheTeam = () => {
   return (
@@ -13,23 +15,23 @@ const TheTeam = () => {
       <div className="profiles flex flex-wrap md:flex-row justify-evenly items-center gap-[70px]">
         <div className="profile flex flex-col items-center gap-2">
           <div className="image lg:w-[130px] lg:h-[130px] w-[150px] h-[150px] overflow-hidden rounded-full">
-            <img src="https://images.pexels.com/photos/1182238/pexels-photo-1182238.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="profile" className='object-cover h-full w-full hover:scale-[102%] ease-in duration-500'/>
+            <img src={ceo} alt="profile" className='object-cover h-full w-full grayscale hover:scale-[102%] ease-in duration-500'/>
           </div>
           <div className="info text-center text-[#1C2451] flex flex-col gap-2">
-            <h2 className='font-bold'>John Doe</h2>
+            <h2 className='font-bold'>Adewale C. Fehintola</h2>
             <p className='text-sm'>Founder/CEO</p>
           </div>
         </div>
         <div className="profile flex flex-col items-center gap-2">
           <div className="image lg:w-[130px] lg:h-[130px] w-[150px] h-[150px] overflow-hidden rounded-full">
-            <img src="https://images.pexels.com/photos/3287016/pexels-photo-3287016.jpeg?auto=compress&cs=tinysrgb&w=600" alt="profile" className='object-cover h-full w-full hover:scale-[102%] ease-in duration-500'/>
+            <img src={headlegal} alt="profile" className='object-cover h-full grayscale w-full hover:scale-[102%] ease-in duration-500'/>
           </div>
           <div className="info text-center text-[#1C2451] flex flex-col gap-2">
-            <h2 className='font-bold'>Vanessa Doe</h2>
-            <p className='text-sm'>Foundeer/CTO</p>
+            <h2 className='font-bold'>Chukwuma Richard Anorue</h2>
+            <p className='text-sm'>Head Legal & Compliance</p>
           </div>
         </div>
-        <div className="profile flex flex-col items-center gap-2">
+        {/* <div className="profile flex flex-col items-center gap-2">
           <div className="image lg:w-[130px] lg:h-[130px] w-[150px] h-[150px] overflow-hidden rounded-full">
             <img src="https://images.pexels.com/photos/3206082/pexels-photo-3206082.jpeg?auto=compress&cs=tinysrgb&w=600" alt="profile" className='object-cover h-full w-full hover:scale-[102%] ease-in duration-500'/>
           </div>
@@ -46,7 +48,7 @@ const TheTeam = () => {
             <h2 className='font-bold'>Jane Oduka</h2>
             <p className='text-sm'>Founder/COO</p>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
