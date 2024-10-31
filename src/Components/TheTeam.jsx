15b0a833884d1b05729f322295c5682e38b47163
Lekan <77983247+XIMalik@users.noreply.github.com>
2024-10-31 14:11:43 +0100

    Update TheTeam.jsx

--- src/Components/TheTeam.jsx
+++ src/Components/TheTeam.jsx
@@ -29,7 +29,7 @@ const TheTeam = () => {
           </div>
           <div className="info text-center text-[#1C2451] flex flex-col gap-2">
             <h2 className='font-bold'>Ademola Adefolaju</h2>
-            <p className='text-sm'>Company Secretary</p>
+            <p className='text-sm'>Head, Legal & Compliance/Company Secretary</p>
           </div>
         </div>
         <div className="profile flex flex-col items-center gap-2">
@@ -38,7 +38,7 @@ const TheTeam = () => {
           </div>
           <div className="info text-center text-[#1C2451] flex flex-col gap-2">
             <h2 className='font-bold'>Chukwuma Richard Anorue</h2>
-            <p className='text-sm'>Head, Legal & Compliance</p>
+            <p className='text-sm'>Executvie Assistant/Legal & Compliance Officer</p>
           </div>
         </div>
         {/* <div className="profile flex flex-col items-center gap-2">
